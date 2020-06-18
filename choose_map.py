fa866d9b5fc46bc7ca35655c52b798a361311dfc
Ben <capnben2009@hotmail.com>
2020-06-18 18:00:14 -0400

    Create choose_map.py

---
+++ choose_map.py
@@ -0,0 +1,25 @@
+
+import random as rando
+import time
+
+maps = ['Reserve', 'Factory' , 'Customs' , 'Interchange' , 'Woods' , 'Shoreline']
+
+#Change this line to True if you have a labs card
+labs = False
+
+if labs == True:
+    maps.append("Labs")
+
+else:
+    print("Continuing")
+
+
+for x in range(10):
+
+
+
+    print("Go fuck yourself")
+
+
+
+    time.sleep(1)
